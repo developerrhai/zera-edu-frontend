eb3d97c09167693723a5bf9a8f830950a4d1cfb9
developerrhai <developerrhai@users.noreply.github.com>
2026-07-10 15:52:15 +0530

    config: change api port to 5005 in api.js

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 // Central API environment configurations
 const API_BASE = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" 
     ? "http://localhost:5000/api" 
-    : "http://zeraedu.com:5000/api";
+    : "http://zeraedu.com:5005/api";
 
 /**
  * Custom fetch client supporting automatic JWT authorization token injection
